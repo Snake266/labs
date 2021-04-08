9634d5eb6631ca1a99dd0e4e7ac2836697c90a22
Baturin Georgii <georgiiba26@gmail.com>
2021-04-08 21:48:24 +0300

    Сделал первое задание 3 лабы

--- second_year/chm/lab_3/src/.auctex-auto/_preamble.el
+++ second_year/chm/lab_3/src/.auctex-auto/_preamble.el
@@ -3,6 +3,8 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "12pt" "a4paper" "oneside")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("babel" "english" "russian")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
@@ -11,11 +13,14 @@
     "article"
     "art12"
     "fontspec"
+    "babel"
     "titling"
     "amsmath"
     "geometry"
     "listings"
-    "color")
+    "color"
+    "graphicx"
+    "float")
    (LaTeX-add-color-definecolors
     "backcolor"))
  :latex)
