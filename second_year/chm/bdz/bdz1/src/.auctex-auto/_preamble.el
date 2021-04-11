c5685e28a5505f1397f54a4872b072d4a9cb6f7c
Baturin Georgii <georgiiba26@gmail.com>
2021-04-11 15:34:57 +0300

    Сделал пятое задание бдз

--- second_year/chm/bdz/bdz1/src/.auctex-auto/_preamble.el
+++ second_year/chm/bdz/bdz1/src/.auctex-auto/_preamble.el
@@ -3,6 +3,9 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "12pt" "a4paper" "oneside")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -12,6 +15,9 @@
     "amsmath"
     "geometry"
     "listings"
-    "color"))
+    "color"
+    "graphicx")
+   (LaTeX-add-color-definecolors
+    "backcolor"))
  :latex)
 
