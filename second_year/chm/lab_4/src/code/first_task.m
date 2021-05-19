c3ac86a46d06081ae46510ba50395aedb50af12c
Baturin Georgii <georgiiba26@gmail.com>
2021-05-19 18:11:20 +0300

    Исправил первое задание четвертой лабы

--- second_year/chm/lab_4/src/code/first_task.m
+++ second_year/chm/lab_4/src/code/first_task.m
@@ -1,5 +1,18 @@
-syms x
+warning('off', 'OctSymPy:sym:rationalapprox')
 f = @(x) sin(x);
-x0 = 0;
+x0 = 1/2;
+
+syms x
+h = 0.5;
+
+v = double(subs(taylor(f,x,x0), x0+h) - subs(taylor(f,x,x0), x0-h))
 
 
+for eps = [10^-3, 10^-6]
+  df = 1;
+  while abs(v-df) >= 10^-3
+    df = (f(x0+h) - f(x0-h))/(2*h);
+    h = h/2;
+  end
+  df
+end
