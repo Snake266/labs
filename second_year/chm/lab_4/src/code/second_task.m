c81b985eab3592883101a35674ad4ea267c65e28
Baturin Georgii <georgiiba26@gmail.com>
2021-05-19 22:12:19 +0300

    Сделал второе задание четвертой лабы

--- second_year/chm/lab_4/src/code/second_task.m
+++ second_year/chm/lab_4/src/code/second_task.m
@@ -2,19 +2,24 @@ f = @(x) sin(x);
 x0 = 1/2;
 syms x;
 
-first_error = [];
-second_error = [];
+R1 = [];
+R2 = [];
 
 h = 1/2;
 v = double(subs(taylor(f,x,x0), x0+h) - subs(taylor(f,x,x0), x0-h))
+n = 10;
+h = 1 ./ [2 : n];
 
-for h = [1/2, 1/4, 1/8, 1/16]
-  df1 = (f(x0 + h) - f(x0))/h;
-  df2 = (f(x0 + h) - f(x0 - h))/(2*h);
+for hi = h
+  df1 = (f(x0 + hi) - f(x0))/hi;
+  df2 = (f(x0 + hi) - f(x0 - hi))/(2*hi);
 
-  first_error = [first_error, abs(df1 - v)];
-  second_error = [second_error, abs(df2 - v)];
+  R1 = [R1, abs(df1 - v)];
+  R2 = [R2, abs(df2 - v)];
 end
 
+hold on, grid on;
+plot(h, R1, '-*b', h, R2, '-*r')
+legend('R1', 'R2')
 df1, df2
-first_error, second_error
+R1, R2
