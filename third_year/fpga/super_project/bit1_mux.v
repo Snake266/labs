6d674f73a943a8cb1e19517f2aa6e0090738d45d
Baturin Georgii <georgiiba26@gmail.com>
2021-09-25 20:59:59 +0300

    Сделал 32-битный АЛУ

--- third_year/fpga/super_project/bit1_mux.v
+++ third_year/fpga/super_project/bit1_mux.v
@@ -1,14 +1,13 @@
 //одно-битный мультиплексер
 module MUX_1(
-           input [31:0]      first,
-           input [31:0]      second,
+           input [31:0]      d0, d1,
            input             sel,
            output reg [31:0] y);
-   always @(first, second, sel) begin
+   always @(*) begin
       case(sel)
-        1'b0: y <= first;
-        1'b1: y <= second;
-        default: y <= first;
+        1'b0: y <= d0;
+        1'b1: y <= d1;
+        default: y <= d0;
       endcase // case (sel)
    end
 endmodule // MUX
--- third_year/fpga/super_project/bit2_mux.v
+++ third_year/fpga/super_project/bit2_mux.v
@@ -1,18 +1,15 @@
 //двух-битный мультиплексер
 module MUX_2(
-           input [31:0]      first,
-           input [31:0]      second,
-           input [31 : 0]    third,
-           input [31:0]      fourth,
+           input [31:0]      d0, d1, d2, d3,
            input [1:0]       sel,
            output reg [31:0] y);
-   always @(first, second, third, fourth, sel) begin
+   always @(*) begin
       case(sel)
-        2'b00: y <= first;
-        2'b01: y <= second;
-        2'b10: y <= third;
-        2'b11: y <= fourth;
-        default: y <= first;
+        2'b00: y <= d0;
+        2'b01: y <= d1;
+        2'b10: y <= d2;
+        2'b11: y <= d3;
+        default: y <= d0;
       endcase // case (sel)
    end
 endmodule // MUX
