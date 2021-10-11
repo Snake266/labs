bb32118d3b1f2fdcb140aefa3e3dbace7be1abc9
Georgii <georgij.baturin@gmail.com>
2021-10-11 14:41:55 +0300

    Обновил АЛУ и добавил тестбенч

--- third_year/fpga/super_project/ALU.v
+++ third_year/fpga/super_project/ALU.v
@@ -1,37 +1,84 @@
+`timescale 1ns / 1ps
+
+// // Обьявление нужных операций
+`define ADD   5'b00000 // сложение
+`define SUB   5'b01000 // вычитание
+`define SLL   5'b00001 // сдвиг влево
+`define LT    5'b00010 // меньше чем
+`define ULT   5'b00011 // безнаковое меньше чем
+`define XOR   5'b00100 // XOR
+`define SRL   5'b00101 // сдвиг вправо
+`define USRL  5'b01101 // беззнаковый сдвиг вправо
+`define OR    5'b00110 // Логический OR
+`define AND   5'b00111 // Логический AND
+`define FEQ   5'b11000 // Равенство с присвоением Flag
+`define FNEQ  5'b11001 // Неравенство с присвоением Flag
+`define FLT   5'b11100 // Меньше чем с присвоением в Flag
+`define FGTE  5'b11101 // Больше или равно с присвоением в Flag
+`define FULT  5'b11110 // Беззнаковое сравнение меньше с присвоением Flag
+`define FUGTE 5'b11111 // Беззнаковое сравнение больше или равно с присвоением Flag
+
 module ALU(
            input [31:0]      A,
            input [31:0]      B,
-           input [2:0]       F,
-           output            Cout,
-           output reg [31:0] Y);
-   // провод, который свяжет 1-битный мультиплексер и сумматор
-   wire [31:0]               mux_1_wire;
-   // 1-битный сумматор, на вход идет ~B и B, управляет этим F[2]
-   MUX_1 mux_1(.d0(~B), .d1(B), .sel(F[2]), .y(mux_1_wire));
+           input [4:0]       F,
+           output reg [31:0] Y,
+           output reg Flag);
+   always @(*) begin
+      case(F)
+        `ADD: begin
+           Y <= A + B;
+           Flag <= 0;
+        end
+        `SUB: begin
+           Y <= A - B;
+           Flag <= 0;
+        end
+        `SLL: begin
+           Y <= A << B;
+           Flag <= 0;
+        end
+        `LT: begin
+           Y <= $signed(A) < $signed(B);
+           Flag <= 0;
+        end
+        `ULT: begin
+           Y <= (A < B);
+           Flag <= 0;
+        end
+        `XOR: begin
+           Y <= A ^ B;
+           Flag <= 0;
+        end
+        `SRL: begin
+           Y <= A >> B;
+           Flag <= 0;
+        end
+        `USRL: begin
+           Y <= $signed(A) >>> $signed (B);
+           Flag <= 0;
+        end
+        `OR: begin
+           Y <= A | B;
+           Flag <= 0;
+        end
+        `AND: begin
+           Y <= A & B;
+           Flag <= 0;
+        end
+        `FEQ: begin
+           Y <= A == B;
+           Flag <= A == B;
+        end
+        `FNEQ: begin
+           Y <= A != B;
+           Flag <= A != B;
+        end
+        `FLT: begin
+           Y <= ($signed(A) < $signed(B));
+           Flag <= ($signed(A) < $signed(B));
+        end
 
-   // Провод, который свяжет сумматор и 2-битный мультиплексор
-   wire [31:0]               adder_wire;
-   bit32_adder adder(.a(A),
-                     .b(mux_1_wire),
-                     .carry_In(F[2]),
-                     .sum(adder_wire),
-                     .carry_Out(Cout));
-
-   // Провода, которые свяжут вентили и мультиплесор
-   wire [31:0]               or_wire;
-   wire [31:0]               and_wire;
-   // Вентили подтягиваются к A и выходу первого мультиплексора
-   // Их выводы подключены ко второму мультиплексору
-   or32_bus or_bus(.a(A), .b(mux_1_wire), or_wire);
-   and32_bus and_bus(.a(A), .b(mux_1_wire), and_wire);
-
-   // Сам мультиплексер
-   MUX_2 mux_2(.d0(and_wire),
-               .d1(or_wire),
-               .d2(adder_wire),
-               .d3(), // Спросить как сделать zero extend и дописать
-               .sel(F[1:0]),
-               .y(Y));
-
-
+      endcase // case (sel)
+   end
 endmodule // ALU
