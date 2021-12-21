0dea7910b4226e5b5adababfa828c4d35f4f6677
Baturin Georgii <georgiiba26@gmail.com>
2021-12-21 15:43:00 +0300

    Обновил instruction_memory

--- third_year/fpga/super_project/instruction_memory.v
+++ third_year/fpga/super_project/instruction_memory.v
@@ -1,9 +1,9 @@
 module instruction_memory(
-                          input [7:0]   A,
+                          input [31:0]   A,
                           output [31:0] RD
                           );
    reg [31:0]                           RAM[0:63];
-   assign RD = RAM[A];
+   assign RD = RAM[A[9:2]];
 
    initial $readmemb("binfile.mem", RAM);
 endmodule
