34f3c5a44fe420de5cac2ea0584a1026c1594cf2
Baturin Georgii <georgiiba26@gmail.com>
2021-12-21 18:58:59 +0300

    Перенос risc-v процессора в отдельный репозиторий

--- third_year/fpga/super_project/instruction_memory.v
+++
@@ -1,9 +0,0 @@
-module instruction_memory(
-                          input [31:0]   A,
-                          output [31:0] RD
-                          );
-   reg [31:0]                           RAM[0:63];
-   assign RD = RAM[A[9:2]];
-
-   initial $readmemb("binfile.mem", RAM);
-endmodule
